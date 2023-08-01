a9d94b5b6f9ede527eb8770f712e2ab4ac81d17a
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-01 21:14:26 +0300

    add content to the angars page

--- src/app/angars/page.tsx
+++ src/app/angars/page.tsx
@@ -1,10 +1,12 @@
 import { CallToAction } from "../components/CallToAction/CallToAction";
+import { AngarsContent } from "../components/Pages/Angars/AngarsContent/AngarsContent";
 import { AngarsHero } from "../components/Pages/Angars/AngarsHero/AngarsHero";
 
 export default function Angars() {
   return (
     <>
       <AngarsHero />
+      <AngarsContent />
       <CallToAction />
     </>
   );
