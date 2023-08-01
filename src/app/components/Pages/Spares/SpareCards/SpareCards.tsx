91a1d29d3358bc980628fc308adfd682d8bd7dda
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-01 20:55:30 +0300

    refactor styles of the cards

--- src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
+++ src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
@@ -35,27 +35,29 @@ export const SparesCards = async () => {
   const cards: Cards = await getData(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*`);
 
   return (
-    <div className={styles.wrapper}>
-      <h2 className={styles.title}>Запчасти и комплектущие</h2>
-      <div className={styles.content}>
-        {cards.data.map((card: Card) => (
-          <article className={styles.card} key={card.id}>
-            <div className={styles.imageContainer}>
-              <Image
-                src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${card.attributes.image.data.attributes.url}`}
-                alt={card.attributes.image.data.attributes.alternativeText}
-                width={300}
-                height={178}
-                className={styles.image}
-              />
-            </div>
-            <div className={styles.cardContent}>
-              <h3>{card.attributes.title}</h3>
-              <p className={styles.price}>{card.attributes.price}</p>
-            </div>
-          </article>
-        ))}
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Запчасти и комплектущие</h2>
+        <div className={styles.content}>
+          {cards.data.map((card: Card) => (
+            <article className={styles.card} key={card.id}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${card.attributes.image.data.attributes.url}`}
+                  alt={card.attributes.image.data.attributes.alternativeText}
+                  width={300}
+                  height={178}
+                  className={styles.image}
+                />
+              </div>
+              <div className={styles.cardContent}>
+                <h3>{card.attributes.title}</h3>
+                <p className={styles.price}>{card.attributes.price}</p>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
