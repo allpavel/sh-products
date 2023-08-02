87286df9aa86970edb24d76c5901d6123e9fb58e
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-02 21:25:41 +0300

    add a pagination to the spares page

--- src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
+++ src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { getData } from "@/app/utils/getData";
 import styles from "./SpareCards.module.css";
+import { LoadButton } from "@/app/components/LoadButton/LoadButton";
 
 interface Card {
   id: number;
@@ -31,32 +35,51 @@ interface Cards {
   };
 }
 
-export const SparesCards = async () => {
-  const cards: Cards = await getData(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*`);
+export const SparesCards = () => {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const response: Cards = await getData(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*&pagination[page]=${pageIndex}&pagination[pageSize]=${process.env.NEXT_PUBLIC_PAGE_SIZE}`
+      );
+      setCards((prev) => [...prev, ...response.data]);
+      setPageCount(response.meta.pagination.pageCount);
+    };
+    fetcher();
+  }, [pageIndex]);
+
+  const handleIncreasePageIndex = () => {
+    setPageIndex((prev) => prev + 1);
+  };
 
   return (
     <section className={styles.container}>
       <div className={styles.wrapper}>
         <h2 className={styles.title}>Запчасти и комплектущие</h2>
         <div className={styles.content}>
-          {cards.data.map((card: Card) => (
-            <article className={styles.card} key={card.id}>
-              <div className={styles.imageContainer}>
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${card.attributes.image.data.attributes.url}`}
-                  alt={card.attributes.image.data.attributes.alternativeText}
-                  width={300}
-                  height={178}
-                  className={styles.image}
-                />
-              </div>
-              <div className={styles.cardContent}>
-                <h3>{card.attributes.title}</h3>
-                <p className={styles.price}>{card.attributes.price}</p>
-              </div>
-            </article>
-          ))}
+          {cards &&
+            cards.map((card: Card) => (
+              <article className={styles.card} key={card.id}>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${card.attributes.image.data.attributes.url}`}
+                    alt={card.attributes.image.data.attributes.alternativeText}
+                    width={300}
+                    height={178}
+                    className={styles.image}
+                  />
+                </div>
+                <div className={styles.cardContent}>
+                  <h3>{card.attributes.title}</h3>
+                  <p className={styles.price}>{card.attributes.price}</p>
+                </div>
+              </article>
+            ))}
         </div>
+        <LoadButton increaseIndex={handleIncreasePageIndex} disable={pageIndex === pageCount} />
         <p className={`${styles.text} ${styles.marginTop}`}>
           У нас Вы можете найти запчасти и комплектующие на сельхозтехнику: разбрасыватели удобрений, штанговые
           (полевые) и вентиляторные (садовые) опрыскиватели, бороны кольчатые и пружинные и многое другое.
