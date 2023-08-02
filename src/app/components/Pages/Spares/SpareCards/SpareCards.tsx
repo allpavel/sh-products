9ec635479a776a2bd1d8e1fb361b526b3f10f079
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-02 22:23:16 +0300

    refactor pagination

--- src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
+++ src/app/components/Pages/Spares/SpareCards/SpareCards.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import { getData } from "@/app/utils/getData";
 import styles from "./SpareCards.module.css";
-import { LoadButton } from "@/app/components/LoadButton/LoadButton";
+import { CardLoader } from "../CardLoader/CardLoader";
 
 interface Card {
   id: number;
@@ -35,25 +32,10 @@ interface Cards {
   };
 }
 
-export const SparesCards = () => {
-  const [cards, setCards] = useState<Card[]>([]);
-  const [pageIndex, setPageIndex] = useState(1);
-  const [pageCount, setPageCount] = useState(0);
-
-  useEffect(() => {
-    const fetcher = async () => {
-      const response: Cards = await getData(
-        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*&pagination[page]=${pageIndex}&pagination[pageSize]=${process.env.NEXT_PUBLIC_PAGE_SIZE}`
-      );
-      setCards((prev) => [...prev, ...response.data]);
-      setPageCount(response.meta.pagination.pageCount);
-    };
-    fetcher();
-  }, [pageIndex]);
-
-  const handleIncreasePageIndex = () => {
-    setPageIndex((prev) => prev + 1);
-  };
+export const SparesCards = async () => {
+  const cards: Cards = await getData(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*&pagination[page]=1&pagination[pageSize]=${process.env.NEXT_PUBLIC_PAGE_SIZE}`
+  );
 
   return (
     <section className={styles.container}>
@@ -61,7 +43,7 @@ export const SparesCards = () => {
         <h2 className={styles.title}>Запчасти и комплектущие</h2>
         <div className={styles.content}>
           {cards &&
-            cards.map((card: Card) => (
+            cards.data.map((card: Card) => (
               <article className={styles.card} key={card.id}>
                 <div className={styles.imageContainer}>
                   <Image
@@ -79,7 +61,7 @@ export const SparesCards = () => {
               </article>
             ))}
         </div>
-        <LoadButton increaseIndex={handleIncreasePageIndex} disable={pageIndex === pageCount} />
+        <CardLoader />
         <p className={`${styles.text} ${styles.marginTop}`}>
           У нас Вы можете найти запчасти и комплектующие на сельхозтехнику: разбрасыватели удобрений, штанговые
           (полевые) и вентиляторные (садовые) опрыскиватели, бороны кольчатые и пружинные и многое другое.
