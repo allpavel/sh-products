8b41af7a946cff944f0355d21887883f983052d7
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-10 19:08:40 +0300

    refactor the spares page

--- src/app/spares/page.tsx
+++ src/app/spares/page.tsx
@@ -1,12 +1,17 @@
 import { Hero } from "@/components/Hero/Hero";
 import { Cards } from "@/components/Cards/Cards";
 import { CallToAction } from "@/components/CallToAction/CallToAction";
+import { getData } from "@/utils/getData";
+import { CardsSchema } from "@/types/types";
 
-export default function Spares() {
+export default async function Spares() {
+  const cards: CardsSchema = await getData(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/spares?populate=*&pagination[page]=1&pagination[pageSize]=${process.env.NEXT_PUBLIC_PAGE_SIZE}`
+  );
   return (
     <>
       <Hero />
-      <Cards pathname="/spares" title="Запчасти и комплектующие" />
+      <Cards cards={cards} pathname="/spares" title="Запчасти и комплектующие" />
       <CallToAction />
     </>
   );
