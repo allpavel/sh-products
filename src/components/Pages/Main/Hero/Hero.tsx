5a13b7dd4d9e761d80ed39d4066417a1f821c263
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-14 21:17:21 +0300

    refactor angars subcategory page

--- src/components/Pages/Main/Hero/Hero.tsx
+++ src/components/Pages/Main/Hero/Hero.tsx
@@ -1,24 +1,20 @@
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { Button } from "../Button/Button";
-import styles from "./HeroTemp.module.css";
+import { Button } from "@/components/Button/Button";
+import styles from "./Hero.module.css";
 
-interface Props {
-  page: string;
-  data: {
-    title: string;
-    metaDescription: string;
-    heroDescription: string;
-  };
-}
-
-export const HeroTemp = ({ page, data }: Props) => {
+export const Hero = () => {
   return (
-    <section className={styles.container} data-subCategory={page}>
+    <section className={styles.container}>
       <div className={styles.filter}>
         <div className={styles.wrapper}>
           <div className={styles.content}>
-            <h1 className={styles.title}>{data.metaDescription}</h1>
-            <ReactMarkdown>{data.heroDescription}</ReactMarkdown>
+            <h1 className={styles.title}>Производство сельскохозяйственной техники и строительство ангаров</h1>
+            <p className={styles.desc}>(Ангары, Бороны, Опрыскиватели, Сельхозтехника, Запчасти)</p>
+            <ul className={styles.list}>
+              <li>Опрыскиватели, бороны, разбрасыватели удобрений</li>
+              <li>
+                Каркасные и бескаркасные арочные сооружения для различных отраслей промышленности и сельского хозяйства
+              </li>
+            </ul>
             <form className={styles.form}>
               <label className={styles.label} htmlFor="tel">
                 Ваш телефон <span className={styles.asterisk}>*</span>
