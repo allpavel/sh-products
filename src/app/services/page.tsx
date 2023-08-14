62477ea5f8e3ef27d64a7b90e8ddc8f9f7c1dded
Pavel Alloyarov <allpavel@yandex.ru>
2023-08-15 00:34:04 +0300

    add hero to the services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,7 @@
-import { ServicesHero } from "@/components/Pages/Services/ServicesHero/ServicesHero";
 import { Cards } from "@/components/Cards/Cards";
 import { ServicesContent } from "@/components/Pages/Services/ServicesContent/ServicesContent";
 import { CallToAction } from "@/components/CallToAction/CallToAction";
+import { Hero } from "@/components/Hero/Hero";
 import { getData } from "@/utils/getData";
 import { CardsSchema } from "@/types/types";
 import type { Metadata } from "next";
@@ -17,7 +17,7 @@ export default async function Services() {
   );
   return (
     <>
-      <ServicesHero />
+      <Hero page="services" data={cards.data[0].attributes.categoryId.data.attributes} />
       <Cards cards={cards} pathname="/services" title="Услуги" />
       <ServicesContent />
       <CallToAction />
